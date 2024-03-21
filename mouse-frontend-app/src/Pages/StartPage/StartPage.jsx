50c4173163c2b8a57bffc536e64e18cd6a82434d
Liana <a>
2024-03-21 19:53:49 +0300

    добавлена часть дизайна для StartPage

--- mouse-frontend-app/src/Pages/StartPage/StartPage.jsx
+++ mouse-frontend-app/src/Pages/StartPage/StartPage.jsx
@@ -1,18 +1,17 @@
-﻿import styles from './StartPage.module.css'
+﻿import styles from "./StartPage.module.css";
 import Button from "./Components/Button/Button.jsx";
-import {button1Text, button2Text, title} from "./Constants/Strings.js";
-
+import { button1Text, button2Text } from "./Constants/Strings.js";
 
 export default function StartPage() {
-    return (
-        <div className={styles.background}>
-            <div className={styles.mainPanel}>
-                <label className={styles.title}>{title}</label>
-                <div className={styles.buttonPanel}>
-                    <Button text={button1Text}></Button>
-                    <Button text={button2Text}></Button>
-                </div>
-            </div>
+  return (
+    <div className={styles.startPage}>
+      <div className={styles.mainPanel}>
+        <h1 className={styles.title1}>Трекер Тренировок</h1>
+        <div className={styles.buttonPanel}>
+          <Button text={button1Text}></Button>
+          <Button text={button2Text}></Button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
