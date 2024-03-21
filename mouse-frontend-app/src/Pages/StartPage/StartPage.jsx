16a8238ce5b3e2e94b20732a1575415759686cce
Liana <a>
2024-03-21 21:06:22 +0300

    добавлен shadow и картинка, которая не отображается

--- mouse-frontend-app/src/Pages/StartPage/StartPage.jsx
+++ mouse-frontend-app/src/Pages/StartPage/StartPage.jsx
@@ -5,6 +5,7 @@ import { button1Text, button2Text } from "./Constants/Strings.js";
 export default function StartPage() {
   return (
     <div className={styles.startPage}>
+    <img src="C:\Users\79991\mouse-front\mouse-frontend-app\public\image.png" alt="аа2у" />
       <div className={styles.mainPanel}>
         <h1 className={styles.title1}>Трекер Тренировок</h1>
         <div className={styles.buttonPanel}>
