15d532aa82d27231061a082334cf9a44dfbb71e0
Liana <a>
2024-03-22 16:27:47 +0300

    добавлены картинки на startPage

--- mouse-frontend-app/src/Pages/StartPage/StartPage.jsx
+++ mouse-frontend-app/src/Pages/StartPage/StartPage.jsx
@@ -1,18 +1,20 @@
 ﻿import styles from "./StartPage.module.css";
 import Button from "./Components/Button/Button.jsx";
-import { button1Text, button2Text } from "./Constants/Strings.js";
+import Pictures from "./Pictures.jsx";
 
 export default function StartPage() {
   return (
-    <div className={styles.startPage}>
-    <img src="C:\Users\79991\mouse-front\mouse-frontend-app\public\image.png" alt="аа2у" />
-      <div className={styles.mainPanel}>
-        <h1 className={styles.title1}>Трекер Тренировок</h1>
-        <div className={styles.buttonPanel}>
-          <Button text={button1Text}></Button>
-          <Button text={button2Text}></Button>
+    <>
+      <Pictures />
+      <div className={styles.startPage}>
+        <div className={styles.mainPanel}>
+          <h1 className={styles.title1}>Трекер Тренировок</h1>
+          <div className={styles.buttonPanel}>
+            <Button text="Регистрация"></Button>
+            <Button text="Вход"></Button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
