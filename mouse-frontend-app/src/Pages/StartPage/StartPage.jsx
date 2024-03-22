f68ab17a25f69f9b05967298b9d4dab0ba5a93a8
Liana <a>
2024-03-22 21:31:48 +0300

    добавлен дизайн для страницы резистрации

--- mouse-frontend-app/src/Pages/StartPage/StartPage.jsx
+++ mouse-frontend-app/src/Pages/StartPage/StartPage.jsx
@@ -1,14 +1,16 @@
 ﻿import styles from "./StartPage.module.css";
 import Button from "./Components/Button/Button.jsx";
 import Pictures from "./Pictures.jsx";
+import CommonStyles from "../../CommonStyles/CommonStyles.module.css"
+
 
 export default function StartPage() {
   return (
     <>
-      <Pictures />
+      {/* <Pictures /> пока в процессе */}
       <div className={styles.startPage}>
         <div className={styles.mainPanel}>
-          <h1 className={styles.title1}>Трекер Тренировок</h1>
+          <h1 className={CommonStyles.title1}>Трекер Тренировок</h1>
           <div className={styles.buttonPanel}>
             <Button text="Регистрация"></Button>
             <Button text="Вход"></Button>
