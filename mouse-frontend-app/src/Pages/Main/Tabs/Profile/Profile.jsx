221e25a85d9bd7513b6e16364842f64a4a18fa3f
Liana <a>
2024-03-23 19:27:44 +0300

    добавлена верстка профиля.

--- mouse-frontend-app/src/Pages/Main/Tabs/Profile/Profile.jsx
+++ mouse-frontend-app/src/Pages/Main/Tabs/Profile/Profile.jsx
@@ -1,3 +1,36 @@
-export default function Profile(){
-    
+import Button from "../../../StartPage/Components/Button/Button";
+import "./Profile.css";
+
+export default function Profile() {
+  return (
+    <div className="user-page-container">
+      <div className="menu">
+        <div className="user-info-header-left">
+          <div className="user-icon"></div>
+          <div className="username">Имя пользователя</div>
+        </div>
+        <div>Расписание</div>
+        <div>Сообщения</div>
+        <div>Мои упражнения</div>
+      </div>
+      <div className="user-info">
+        <div className="personal-data">
+          <div className="personal-data-title">Персональные данные</div>
+          <div className="user-photo"></div>
+          <button>Изменить профиль</button>
+          <div className="user-name">Имя: Имя пользователя</div>
+          <div className="user-dob">Дата рождения: Дата рождения</div>
+          <div className="physical-data">Физические данные</div>
+          <div className="physical-data-item">Рост: Рост пользователя</div>
+          <div className="physical-data-item">Вес: Вес пользователя</div>
+          <div className="contacts">Контакты</div>
+          <div className="contacts-item">Телефон: Номер телефона</div>
+          <div className="contacts-item">Email: email@example.com</div>
+        </div>
+        <div className="fill-profile-button">
+          <Button text="Заполнить профиль"></Button>
+        </div>
+      </div>
+    </div>
+  );
 }
