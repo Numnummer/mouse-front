fc755969caf7b0a418305e872cb8f5816b622527
Akhmed Mirsaitov <bignumbergames@gmail.com>
2024-03-23 14:50:02 +0300

    Добавил систему табов в основную страницу

--- mouse-frontend-app/src/Pages/Main/Main.jsx
+++ mouse-frontend-app/src/Pages/Main/Main.jsx
@@ -1,15 +1,35 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+import Profile from "./Tabs/Profile/Profile";
+import Schedule from "./Tabs/Schedule/Schedule";
+import MyExcercises from './Tabs/MyExcercises/MyExcercises'
+import Messages from './Tabs/Messages/Messages'
 
 export default function Main(){
-    let token=localStorage.getItem('jwtToken')
-    useEffect(()=>{
-
-    },[])
+    let token=localStorage.getItem('jwtToken')        
     if (!token) {
         return <>
             <h1>Войдите в систему</h1>
             <button>Войти</button>
         </>;
+    }
+
+    const [currentTab,setCurrentTab]=useState(localStorage.getItem('currentTab'))
+    useEffect(()=>{
+        localStorage.setItem('currentTab',currentTab)
+    },[currentTab])
+    let currentTabComponent=<Profile></Profile>
+    switch (currentTab) {
+        case 'Schedule':
+            currentTabComponent=<Schedule></Schedule>
+            break;
+        case 'MyExcercises':
+            currentTabComponent=<MyExcercises></MyExcercises>
+            break;  
+        case 'Messages':
+            currentTabComponent=<Messages></Messages>
+            break;    
+        default:
+            break;
     }
 
     return (
