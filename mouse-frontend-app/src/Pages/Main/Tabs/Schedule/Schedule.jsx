f252250f0f785d299fbf7f711055c660df0e400e
Liana <a>
2024-04-02 13:27:54 +0300

    добавлена модалка на добавление тренировки.

--- mouse-frontend-app/src/Pages/Main/Tabs/Schedule/Schedule.jsx
+++ mouse-frontend-app/src/Pages/Main/Tabs/Schedule/Schedule.jsx
@@ -1,16 +1,42 @@
-import Button from "../../../StartPage/Components/Button/Button";
+// import Button from "../../../StartPage/Components/Button/Button";
 import Calendar from "./Components/Calendar/Calendar";
-import "./Schedule.css"
+import "./Schedule.css";
+import { useState } from "react";
+import { Button, Modal } from "antd";
+import { DatePicker } from 'antd';
 
 export default function Schedule() {
+  const [open, setOpen] = useState(false);
+  const onChange = (date, dateString) => {
+    console.log(date, dateString);
+  };
+
   return (
     <div className="user-page-container">
       <div className="user-calendar">
         <Calendar />
       </div>
-      <div className="fill-profile-button">
-        <Button text={'Добавить тренировку'}></Button>
-      </div>
+      <Button className="add-training" onClick={() => setOpen(true)}>
+        Добавить тренировку
+      </Button>
+      <Modal
+        title="Добавить тренировку"
+        open={open}
+        onOk={() => setOpen(false)}
+        onCancel={() => setOpen(false)}
+        width={600}
+        footer={(_, { CancelBtn }) => (
+          <>
+            <Button type="primary">Добавить тренировку</Button>
+            <CancelBtn />
+          </>
+        )}
+      >
+        <div className="">
+          <input className="inputs" placeholder={"Наименование тренировки"}></input>
+          <DatePicker className="date-picker" onChange={onChange} needConfirm />
+        </div>        
+      </Modal>
     </div>
   );
 }
