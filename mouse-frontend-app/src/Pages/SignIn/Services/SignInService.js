c57e2e68b7ddbc81d03e1894d2cefd55fa3af7ef
Akhmed Mirsaitov <bignumbergames@gmail.com>
2024-04-10 18:33:41 +0300

    Исправил ошибку при входе

--- mouse-frontend-app/src/Pages/SignIn/Services/SignInService.js
+++ mouse-frontend-app/src/Pages/SignIn/Services/SignInService.js
@@ -2,9 +2,13 @@ import { isValidSignInData } from "../../../CommonServices/ValidationService.js"
 import { signIn } from "../../../Api/Auth/SignIn.js";
 
 export function processSignIn(signInData) {
-  if (isValidSignInData(signInData)) {
-    return signIn(signInData);
-  }
+  return new Promise((resolve, reject) => {
+    if (isValidSignInData(signInData)) {
+      resolve(signIn(signInData));
+    } else {
+      reject("Invalid sign-in data");
+    }
+  });
 }
 
 export function processSignInByOtherService(service) {}
