f056a26e4ed02074e5c63e7e38f60ccf430a0a66
Liana <a>
2024-04-14 23:06:08 +0300

    fix

--- mouse-frontend-app/src/Pages/Main/Tabs/Schedule/Tabs/TrainingInfo.jsx
+++ mouse-frontend-app/src/Pages/Main/Tabs/Schedule/Tabs/TrainingInfo.jsx
@@ -56,89 +56,91 @@ export default function TrainingInfo({
 
   return (
     <div className="user-page-container">
-      <ToastContainer
-        position="top-center"
-        autoClose={2000}
-        limit={2}
-      ></ToastContainer>
-      {/* <Button onClick={() => setCurrentTab("Main")}>Назад</Button> */}
-      <div className="training-info">
-        <label className="training-date">{format(date, "d MMMM yyyy", { locale: ru })}</label>
-        <label className="training-name">{training.name}</label>
-      </div>
-      <div>
-        <div className="training-types">
-          <label>Наименование упражнения</label>
-          <label>Количество повторений</label>
-          <label>Количество подходов</label>
+      <div className="user-page-container-training">
+        <ToastContainer
+          position="top-center"
+          autoClose={2000}
+          limit={2}
+        ></ToastContainer>
+        {/* <Button onClick={() => setCurrentTab("Main")}>Назад</Button> */}
+        <div className="training-info">
+          <label className="training-date">{format(date, "d MMMM yyyy", { locale: ru })}</label>
+          <label className="training-name">{training.name}</label>
         </div>
         <div>
-          {excercises.map((excercise, index) => {
-            return (
-              <div
-                key={index}
-                onClick={() => {
-                  setCurrentTab("ExcerciseInfo");
-                  setCurrentExcercise(excercise);
-                }}
-              >
-                <label>{excercise.name}</label>
-                <label>{excercise.repetitions}</label>
-                <label>{excercise.approaches}</label>
-              </div>
-            );
-          })}
+          <div className="training-types">
+            <label>Наименование упражнения</label>
+            <label>Количество повторений</label>
+            <label>Количество подходов</label>
+          </div>
+          <div>
+            {excercises.map((excercise, index) => {
+              return (
+                <div
+                  key={index}
+                  onClick={() => {
+                    setCurrentTab("ExcerciseInfo");
+                    setCurrentExcercise(excercise);
+                  }}
+                >
+                  <label>{excercise.name}</label>
+                  <label>{excercise.repetitions}</label>
+                  <label>{excercise.approaches}</label>
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div>
 
-      <Modal
-        open={open}
-        onOk={onModalOk}
-        onCancel={() => setOpen(false)}
-        width={600}
-        footer={(_, { CancelBtn }) => (
-          <>
-            <Button type="primary" onClick={onModalOk}>
-              Добавить упражнение
-            </Button>
-            <CancelBtn />
-          </>
-        )}
-      >
-        <div className="">
-          <input
-            name={"description"}
-            onChange={handleInputChange}
-            className="inputs"
-            placeholder={"Описание"}
-          ></input>
-          <input
-            name={"implementationProgress"}
-            onChange={handleInputChange}
-            className="inputs"
-            placeholder={"Ход выполнения"}
-          ></input>
-          <input
-            name={"explanationVideo"}
-            onChange={handleInputChange}
-            className="inputs"
-            placeholder={"Ссылка на видео"}
-          ></input>
-          <input
-            name={"repetitions"}
-            onChange={handleInputChange}
-            className="inputs"
-            placeholder={"Количество повторений"}
-          ></input>
-          <input
-            name={"approaches"}
-            onChange={handleInputChange}
-            className="inputs"
-            placeholder={"Количество подходов"}
-          ></input>
-        </div>
-      </Modal>
-      <Button className="add-training-button" onClick={() => setOpen(true)}>Добавить упражнение</Button>
+        <Modal
+          open={open}
+          onOk={onModalOk}
+          onCancel={() => setOpen(false)}
+          width={600}
+          footer={(_, { CancelBtn }) => (
+            <>
+              <Button type="primary" onClick={onModalOk}>
+                Добавить упражнение
+              </Button>
+              <CancelBtn />
+            </>
+          )}
+        >
+          <div className="">
+            <input
+              name={"description"}
+              onChange={handleInputChange}
+              className="inputs"
+              placeholder={"Описание"}
+            ></input>
+            <input
+              name={"implementationProgress"}
+              onChange={handleInputChange}
+              className="inputs"
+              placeholder={"Ход выполнения"}
+            ></input>
+            <input
+              name={"explanationVideo"}
+              onChange={handleInputChange}
+              className="inputs"
+              placeholder={"Ссылка на видео"}
+            ></input>
+            <input
+              name={"repetitions"}
+              onChange={handleInputChange}
+              className="inputs"
+              placeholder={"Количество повторений"}
+            ></input>
+            <input
+              name={"approaches"}
+              onChange={handleInputChange}
+              className="inputs"
+              placeholder={"Количество подходов"}
+            ></input>
+          </div>
+        </Modal>
+        <Button className="add-training-button" onClick={() => setOpen(true)}>Добавить упражнение</Button>
+      </div>
     </div>
   );
 }
