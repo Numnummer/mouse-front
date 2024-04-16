8c1e602397ed29aef096d8894722338738b3e4da
Liana <a>
2024-04-16 11:33:57 +0300

    карточка упражнения, дизайн

--- mouse-frontend-app/src/Pages/Main/Tabs/Schedule/Tabs/ExcerciseInfo.jsx
+++ mouse-frontend-app/src/Pages/Main/Tabs/Schedule/Tabs/ExcerciseInfo.jsx
@@ -1,14 +1,25 @@
-export default function ({ excercise, setCurrentTab }) {
+import "./ExcerciseInfo.css";
+import image from "../../../../../../public/video-image.png"; 
+
+export default function ExerciseInfo({ excercise, setCurrentTab }) {
   return (
-    <div>
-      <button onClick={() => setCurrentTab("TrainingInfo")}>Назад</button>
-      <label>{excercise.name}</label>
-      <p>{excercise.description}</p>
-      <label>Количество подходов: {excercise.approaches}</label>
-      <label>Количество повторений: {excercise.repetitions}</label>
-      <label>Ход выполнения</label>
-      <p>{excercise.implementationProgress}</p>
-      <div>{excercise.explanationVideo}</div>
+    <div className="user-page-container">
+      <div className="exercise-container">
+        {/* <button onClick={() => setCurrentTab("TrainingInfo")}>Назад</button> */}
+        <label className="excercise-name">{excercise.name}</label>
+        <p className="excercise-description">{excercise.description}</p>
+        <div className="excercise-types">
+          <label>Количество подходов: {excercise.approaches}</label>
+          <label>Количество повторений: {excercise.repetitions}</label>
+        </div>
+        <div className="hod">
+          <label>Ход выполнения</label>
+          <p>{excercise.implementationProgress}</p>
+        </div>
+          <a className="exercise-video-" href={excercise.explanationVideo}>
+            <img width='422px' className="exercise-img-" src={image} />
+          </a>
+      </div>
     </div>
   );
 }
