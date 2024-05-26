180ce3b86bcccb6a2db5a8e4cdb96badd4bb22bc
Akhmed Mirsaitov <bignumbergames@gmail.com>
2024-05-26 16:05:58 +0300

    обновил урлы

--- mouse-frontend-app/src/Constants/Messages.js
+++ mouse-frontend-app/src/Constants/Messages.js
@@ -1,2 +1,2 @@
-//export const hubConnectionUrl = "http://192.144.12.224:8080/notification";
-export const hubConnectionUrl = "https://localhost:7267/notification";
+export const hubConnectionUrl = "http://192.144.12.224:8080/notification";
+//export const hubConnectionUrl = "https://localhost:7267/notification";
